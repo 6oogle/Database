2dd41d4f32f4affbfcd288bda9fb21aa3da9309b
6oogle <32604831+6oogle@users.noreply.github.com>
2018-08-08 02:53:48 +0300

    Add byte zip/unzip

---
+++ src/__google_/util/ByteZip.java
@@ -0,0 +1,59 @@
+package __google_.util;
+
+public class ByteZip {
+	private final ByteArrayNodeList list = new ByteArrayNodeList();
+
+	public byte[] build(){
+		byte result[] = new byte[size()];
+		int write = 0;
+		for(int i = 0; i < list.size(); i++){
+			byte array[] = list.get(i);
+			byte size[] = Coder.getSize(array.length);
+			result = Coder.addBytes(size, write, result);
+			write = write + size.length;
+			result = Coder.addBytes(array, write, result);
+			write = write + array.length;
+		}
+		return result;
+	}
+
+	public ByteZip add(String object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(boolean object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(long object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(int object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(short object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(byte object){
+		return add(Coder.toBytes(object));
+	}
+
+	public ByteZip add(byte array[]){
+		list.add(array);
+		return this;
+	}
+
+	public int size(){
+		int size = list.size();
+		for(int i = 0; i < list.size(); i++){
+			byte array[] = list.get(i);
+			size = size + array.length;
+			if(array.length > 126)size = size + 4;
+			//Add integer size
+		}
+		return size;
+	}
+}

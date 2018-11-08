277d7c84bfe52ced7d093601341c27fb8560308a
oogle <anartvel@gmail.com>
2018-11-09 02:42:32 +0300

    add methods fileio

--- src/__google_/util/FileIO.java
+++ src/__google_/util/FileIO.java
@@ -1,14 +1,26 @@
 package __google_.util;
 
 import java.io.*;
+import java.util.List;
 
 public class FileIO {
     //Can be set something, for libraries
     public static String prefix = "AppData/";
 
-    public static void writeBytes(String strFile, byte[] array){
-        File file = getFile(strFile);
-        if(!file.exists())create(strFile);
+    public static void writeByteable(String strFile, Byteable byteable){
+        writeByteable(getFile(strFile), byteable);
+    }
+
+    public static void writeByteable(File file, Byteable byteable){
+        writeBytes(file, byteable.toBytes());
+    }
+
+    public static void writeBytes(String strFile, byte array[]){
+        writeBytes(getFile(strFile), array);
+    }
+
+    public static void writeBytes(File file, byte array[]){
+        if(!file.exists())create(file);
         close(Exceptions.getThrowsEx(() -> {
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
             out.write(array);
@@ -16,9 +28,20 @@ public class FileIO {
         }));
     }
 
+    public static void writeList(String strFile, List<String> list){
+        writeList(getFile(strFile), list);
+    }
+
+    public static void writeList(File file, List<String> list){
+        write(file, Coder.toString(list));
+    }
+
     public static void write(String strFile, String write){
-        File file = getFile(strFile);
-        if(!file.exists())create(strFile);
+        write(getFile(strFile), write);
+    }
+
+    public static void write(File file, String write){
+        if(!file.exists())create(file);
         close(Exceptions.getThrowsEx(() -> {
             BufferedWriter out = new BufferedWriter(new FileWriter(file));
             out.write(write);
@@ -26,8 +49,19 @@ public class FileIO {
         }));
     }
 
+    public static <T extends Byteable> T readByteable(String strFile, Class<T> clazz){
+        return readByteable(getFile(strFile), clazz);
+    }
+
+    public static <T extends Byteable> T readByteable(File file, Class<T> clazz){
+        return Byteable.toByteable(readBytes(file), clazz);
+    }
+
     public static byte[] readBytes(String strFile){
-        File file = getFile(strFile);
+        return readBytes(getFile(strFile));
+    }
+
+    public static byte[] readBytes(File file){
         if(!file.exists())return null;
         byte array[] = new byte[(int)file.length()];
         close(Exceptions.getThrowsEx(() -> {
@@ -38,8 +72,19 @@ public class FileIO {
         return array;
     }
 
+    public static List<String> readList(String strFile){
+        return readList(getFile(strFile));
+    }
+
+    public static List<String> readList(File file){
+        return Coder.toList(read(file));
+    }
+
     public static String read(String strFile){
-        File file = getFile(strFile);
+        return read(getFile(strFile));
+    }
+
+    public static String read(File file){
         if(!file.exists())return null;
         StringBuilder buffer = new StringBuilder((int)file.length());
         close(Exceptions.getThrowsEx(() -> {
@@ -55,7 +100,10 @@ public class FileIO {
     }
 
     public static void create(String strFile){
-        File file = getFile(strFile);
+        create(getFile(strFile));
+    }
+
+    public static void create(File file){
         Exceptions.runThrowsEx(() -> {
             file.getParentFile().mkdirs();
             file.getParentFile().mkdir();
